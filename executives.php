3f7665ef8c30f32c7d307dce526b28773abf00e6
Tijani Moro <tjhackx111@gmail.com>
2023-08-22 02:08:55 +0000

    update

--- executives.php
+++ executives.php
@@ -16,7 +16,7 @@
     <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" style="background: linear-gradient(0deg, #616161, #424242a8), url(<?= PROOT; ?>dist/media/bg-1.jpg); background-position: center; background-repeat: no-repeat; background-size: cover;">
         <div class="col-lg-6 px-0">
             <h1 class="display-4 fst-italic">Know your executives</h1>
-            <p class="lead my-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eum omnis mollitia adipisci, sed veniam in consequatur totam obcaecati, facere ipsam quas et assumenda velit esse repudiandae voluptate laboriosam incidunt!</p>
+            <p class="lead my-3">It’s fairly understood that great leaders don’t lead processes; they lead people.</p>
             <p class="lead mb-0"><a href="<?= PROOT ; ?>" class="text-body-emphasis fw-bold">Go back home...</a></p>
         </div>
     </div>
@@ -29,7 +29,7 @@
                         <div class="card">
                             <img src="<?= PROOT . $row["membership_passport"]; ?>" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title"><?= ucwords($row["membership_fname"] . ' ' . $row["membership_lname"]); ?></h5>
+                                <h5 class="card-title"><?= strtoupper($row["membership_fname"] . ' ' . $row["membership_lname"]); ?></h5>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item"><?= ucwords($row["membership_position"]); ?></li>
